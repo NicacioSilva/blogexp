4f531a9305003a0be093beb15df82887dce70aa3
NicacioSilva <nas@cesar.school>
2020-05-20 19:07:30 -0300

    add some fields to forms.

--- users/forms.py
+++ users/forms.py
@@ -5,10 +5,14 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    first_name = forms.CharField(max_length=20)
+    last_name = forms.CharField(max_length=20)
 
     class Meta:
         model = User
         fields = ['username',
+                  'first_name',
+                  'last_name',
                   'email',
                   'password1',
                   'password2']
